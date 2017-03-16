ef9ae02799b04e47b18cd18ab543088203ce186e
agdhruv <dhruv_agarwal@live.com>
2017-03-17 00:01:11 +0530

    Adds mains functionality.

--- app.py
+++ app.py
@@ -38,18 +38,48 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    message_time = entry["time"] # time at which message was sent in UNIX Time
 
                     if message_text=="SOMETHING":
+
+                        # Get current time - time at which message has been received by this script
+                        from datetime import datetime, timedelta
+                        my_time = datetime.utcnow() + timedelta(hours=5) + timedelta(minutes=30)
+
+                        # Start generating return message:
+                        return_message = "The request was received at " + my_time.strftime('%H:%M') + "\n"
+
+                        # Convert to integer
+                        my_time = int(my_time.strftime('%H%M'))
+
                         # Make list with times of shuttles
-                        # times_campus = [630,700,730,800,820,845,900,930,1000,1100,1200,1300,1400,1500]
+                        times_campus = [630,700,730,800,820,845,900,930,1000,1100,1200,1300,1400,1500,1600,1630,1700,1720,1740,1800,1820,1840,1900,1930,2000,2030,2100,2200,-1]
 
-                        # Get message_time in normal form like 2250, 1110 etc
-                        import datetime
-                        c_time = message_time/1000
-                        my_time = datetime.datetime.fromtimestamp(c_time).strftime('%H%M')
+                        # Use binary search to search for next 3 shuttles
+                        low = 0
+                        high = len(times_campus) - 1
 
-                        send_message(sender_id, "what i expected"+ str(my_time))
+                        toCheck = my_time
+
+                        while low<high:
+                            mid = (low + high)/2
+                            if times_campus[mid]==toCheck:
+                                low = mid
+                                break
+                            elif times_campus[mid]>toCheck:
+                                high = mid
+                            elif times_campus[mid]<toCheck:
+                                low = mid + 1
+                        ans_index = low
+
+                        for i in range(low,low+3):
+                            if times_campus[i] == -1:
+                                return_message += "\nNo more shuttles today."
+                                break
+                            next_shuttle = str(times_campus[i])
+                            next_shuttle = next_shuttle[:-2] + ":" + next_shuttle[-2:]
+                            return_message += "\n" + next_shuttle
+
+                        send_message(sender_id, return_message)
                     else:
                         send_message(sender_id, "Don't know what you are talking about.")
 
