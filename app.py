45e9795c3ccddc802248292ec1dc5e40f53c8cbf
agdhruv <dhruv_agarwal@live.com>
2017-03-17 01:13:59 +0530

    Link to policy added.

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def verify():
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
-    return "Hello world", 200
+    return 'Hello world<br><a href="privacy_policy.txt">Privacy Policy</a>', 200
 
 
 @app.route('/', methods=['POST'])
